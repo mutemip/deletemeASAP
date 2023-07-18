b8b9921300960b5b8f4c7425ac2f546985471e1f
mutemip <paulmutemi2030@gmail.com>
2023-07-18 19:39:52 +0300

    asyncio test notes

--- asyncio_script.py
+++ asyncio_script.py
@@ -1,3 +1,12 @@
+##The Asyncio Module
+# Now we will cover another concurrent programming model: the asyncio module.
+
+# The asyncio module uses async/await syntax. async and await are two keywords that allow you to build and execute asynchronous code in your programs.
+
+# The async keyword declares a function as a coroutine. Coroutines are functions that may return normally with a value or may suspend themselves internally and return a continuation. This is a fancy way of saying they allow tasks to be paused and resumed to mimic multitasking. This is conceptually very similar to what we saw with threads! Coroutines are at the heart of asynchronous programs in Python.
+
+# The await keyword suspends execution of the current task until whatever is being “await”ed on is completed. For example, if we have an “await function task2” within a coroutine “task1” this tells Python “Suspend task1 until task2 is completed.”
+
 import time
 import asyncio
 
